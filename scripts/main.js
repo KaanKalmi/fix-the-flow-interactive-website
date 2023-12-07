d555223dc87184e86881b1a48624c73d74edefdd
KaanKalmi <kaan.kalmioglu@hva.nl>
2023-12-07 12:13:51 +0100

    start on animating cards

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,8 @@
-let btnInteraction = document.querySelector('button');
+let shuffle = document.querySelector('button');
+
+shuffle.addEventListener('click', shuffle)
+shuffle.addEventListener('animationend', shuffle)
+
+function shuffle() {
+    shuffle.classList.toggle('shuffle');
+}
