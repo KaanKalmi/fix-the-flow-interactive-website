be3c82dbd5e144f72b8c106dbbcc9b2cc7f702c7
KaanKalmi <kaan.kalmioglu@hva.nl>
2023-12-08 12:55:43 +0100

    card animation fix JS
    Gefixt door Tristan!!! ik kreeg wel uitleg over de code en wat het probleem !

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,31 @@
-let interaction = document.querySelector('input')
 
-interaction.addEventListener('click', shuffle)
-interaction.addEventListener('animationend', shuffle)
+let interaction = document.querySelector('radio')
+
+let card1 = document.querySelector('#card1')
+let radioCard1 = document.getElementsByName('1')
+let cards = document.querySelectorAll('.card')
+
+console.log(card1);
+console.log(radioCard1);
+console.log(cards);
+
+radioCard1.forEach(radioElement => {
+   radioElement.addEventListener('click', function() {
+      console.log('clicked card');   
+      card1.classList.toggle('shuffle')
+   })
+});
+
+// card1.addEventListener('click', function() {
+//    console.log('clicked card');
+// })
+
+// radioCard1.addEventListener('onchange', shuffle()) 
+
+// interaction.addEventListener('click', shuffle())
+// interaction.addEventListener('animationend', shuffle)
 
 function shuffle() {
-   interaction.classList.toggle('shuffle')
+   console.log('clicked card');
+   card1.classList.toggle('shuffle')
 }
