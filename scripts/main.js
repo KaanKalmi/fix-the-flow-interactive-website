4545c93929df99b691839531cf0b21f1c6dcfb41
KaanKalmi <kaan.kalmioglu@hva.nl>
2023-12-08 13:32:01 +0100

    fixed

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,3 @@
-
-let interaction = document.querySelector('radio')
-
 let card1 = document.querySelector('#card1')
 let radioCard1 = document.getElementsByName('1')
 let cards = document.querySelectorAll('.card')
@@ -13,19 +10,47 @@ radioCard1.forEach(radioElement => {
    radioElement.addEventListener('click', function() {
       console.log('clicked card');   
       card1.classList.toggle('shuffle')
+      card1.addEventListener('animationend', function() {
+         card1.classList.toggle('hide')
+      })
    })
 });
-
-// card1.addEventListener('click', function() {
-//    console.log('clicked card');
-// })
-
-// radioCard1.addEventListener('onchange', shuffle()) 
-
-// interaction.addEventListener('click', shuffle())
-// interaction.addEventListener('animationend', shuffle)
 
 function shuffle() {
    console.log('clicked card');
    card1.classList.toggle('shuffle')
 }
+
+let card2 = document.querySelector('#card2')
+let radioCard2 = document.getElementsByName('2')
+
+console.log(card2);
+console.log(radioCard2);
+
+radioCard2.forEach(radioElement => {
+   radioElement.addEventListener('click', function() {
+      console.log('clicked card');   
+      card2.classList.toggle('shuffle')
+      card2.addEventListener('animationend', function() {
+         card2.classList.toggle('hide')
+      })
+   })
+}
+);
+
+let card3 = document.querySelector('#card3')
+let radioCard3 = document.getElementsByName('3')
+
+console.log(card3);
+console.log(radioCard3);
+
+radioCard3.forEach(radioElement => {
+   radioElement.addEventListener('click', function() {
+      console.log('clicked card');   
+      card3.classList.toggle('shuffle')
+      card3.addEventListener('animationend', function() {
+         card3.classList.toggle('hide')
+      })
+   })
+}
+);
