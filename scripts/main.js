593abd3870bfb1dbc7feec2fa0366f014ec35140
Kaan <kaan.kalmioglu@hva.nl>
2023-12-09 16:16:21 +0100

    lets not go all out on the cards right now
    reduced card count from 15 to 5, last update on cards for week 1, next update will be content for home page!

--- scripts/main.js
+++ scripts/main.js
@@ -1,259 +1,89 @@
-let card1 = document.querySelector('#card1')
-let radioCard1 = document.getElementsByName('1')
-let cards = document.querySelectorAll('.card')
+let card1 = document.querySelector('#card1') // select the first card
+let radioCard1 = document.getElementsByName('1') // select the radio buttons for the first card
+let cards = document.querySelectorAll('.card') // select all cards
 
-console.log(card1);
-console.log(radioCard1);
-console.log(cards);
+console.log(card1); // shows card1 in console (inspect element)
+console.log(radioCard1); // shows racioCard1 in console (inspect element)
+console.log(cards); // shows cards in console (inspect element)
 
-radioCard1.forEach(radioElement => {
-   radioElement.addEventListener('click', function () {
-      console.log('clicked card');
-      card1.classList.toggle('shuffle')
-      card1.addEventListener('animationend', function () {
-         card1.classList.toggle('hide')
+radioCard1.forEach(radioElement => { // for each radio button
+   radioElement.addEventListener('click', function () { // add event listener
+      console.log('clicked card'); // shows clicked card in console (inspect element)
+      card1.classList.toggle('shuffle') // toggle shuffle class
+      card1.addEventListener('animationend', function () { // add event listener
+         card1.classList.toggle('hide') // toggle hide class
       })
    })
 });
 
-function shuffle() {
-   console.log('clicked card');
-   card1.classList.toggle('shuffle')
+function shuffle() { // shuffle function
+   console.log('clicked card'); // shows clicked card in console (inspect element)
+   card1.classList.toggle('shuffle') // toggle shuffle class
 }
 
-let card2 = document.querySelector('#card2')
-let radioCard2 = document.getElementsByName('2')
+let card2 = document.querySelector('#card2') // select the second card
+let radioCard2 = document.getElementsByName('2') // select the radio buttons for the second card
 
-console.log(card2);
-console.log(radioCard2);
+console.log(card2); // shows card2 in console (inspect element)
+console.log(radioCard2); // shows racioCard2 in console (inspect element)
 
-radioCard2.forEach(radioElement => {
-   radioElement.addEventListener('click', function () {
-      console.log('clicked card');
-      card2.classList.toggle('shuffle')
-      card2.addEventListener('animationend', function () {
-         card2.classList.toggle('hide')
+radioCard2.forEach(radioElement => { // for each radio button
+   radioElement.addEventListener('click', function () { // add event listener
+      console.log('clicked card'); // shows clicked card in console (inspect element)
+      card2.classList.toggle('shuffle') // toggle shuffle class
+      card2.addEventListener('animationend', function () { // add event listener
+         card2.classList.toggle('hide') // toggle hide class
+      })
+   })
+}
+);
+ 
+let card3 = document.querySelector('#card3') // select the third card
+let radioCard3 = document.getElementsByName('3') // select the radio buttons for the third card
+
+console.log(card3); // shows card3 in console (inspect element)
+console.log(radioCard3); // shows racioCard3 in console (inspect element)
+
+radioCard3.forEach(radioElement => { // for each radio button
+   radioElement.addEventListener('click', function () { // add event listener
+      console.log('clicked card'); // shows clicked card in console (inspect element)
+      card3.classList.toggle('shuffle') // toggle shuffle class
+      card3.addEventListener('animationend', function () { // add event listener
+         card3.classList.toggle('hide')  // toggle hide class
       })
    })
 }
 );
 
-let card3 = document.querySelector('#card3')
-let radioCard3 = document.getElementsByName('3')
+let card4 = document.querySelector('#card4') // select the fourth card
+let radioCard4 = document.getElementsByName('4') // select the radio buttons for the fourth card
 
-console.log(card3);
-console.log(radioCard3);
+console.log(card4); // shows card4 in console (inspect element)
+console.log(radioCard4); // shows racioCard4 in console (inspect element)
 
-radioCard3.forEach(radioElement => {
-   radioElement.addEventListener('click', function () {
-      console.log('clicked card');
-      card3.classList.toggle('shuffle')
-      card3.addEventListener('animationend', function () {
-         card3.classList.toggle('hide')
+radioCard4.forEach(radioElement => { // for each radio button
+   radioElement.addEventListener('click', function () { // add event listener
+      console.log('clicked card'); // shows clicked card in console (inspect element)
+      card4.classList.toggle('shuffle') // toggle shuffle class
+      card4.addEventListener('animationend', function () { // add event listener
+         card4.classList.toggle('hide') // toggle hide class
       })
    })
 }
 );
 
-let card4 = document.querySelector('#card4')
-let radioCard4 = document.getElementsByName('4')
+let card5 = document.querySelector('#card5') // select the fifth card
+let radioCard5 = document.getElementsByName('5') // select the radio buttons for the fifth card
 
-console.log(card4);
-console.log(radioCard4);
+console.log(card5); // shows card5 in console (inspect element)
+console.log(radioCard5); // shows racioCard5 in console (inspect element)
 
-radioCard4.forEach(radioElement => {
-   radioElement.addEventListener('click', function () {
-      console.log('clicked card');
-      card4.classList.toggle('shuffle')
-      card4.addEventListener('animationend', function () {
-         card4.classList.toggle('hide')
-      })
-   })
-}
-);
-
-let card5 = document.querySelector('#card5')
-let radioCard5 = document.getElementsByName('5')
-
-console.log(card5);
-console.log(radioCard5);
-
-radioCard5.forEach(radioElement => {
-   radioElement.addEventListener('click', function () {
-      console.log('clicked card');
-      card5.classList.toggle('shuffle')
-      card5.addEventListener('animationend', function () {
-         card5.classList.toggle('hide')
-      })
-   })
-}
-);
-
-let card6 = document.querySelector('#card6')
-let radioCard6 = document.getElementsByName('6')
-
-console.log(card6);
-console.log(radioCard6);
-
-radioCard6.forEach(radioElement => {
-   radioElement.addEventListener('click', function () {
-      console.log('clicked card');
-      card6.classList.toggle('shuffle')
-      card6.addEventListener('animationend', function () {
-         card6.classList.toggle('hide')
-      })
-   })
-}
-);
-
-let card7 = document.querySelector('#card7')
-let radioCard7 = document.getElementsByName('7')
-
-console.log(card7);
-console.log(radioCard7);
-
-radioCard7.forEach(radioElement => {
-   radioElement.addEventListener('click', function () {
-      console.log('clicked card');
-      card7.classList.toggle('shuffle')
-      card7.addEventListener('animationend', function () {
-         card7.classList.toggle('hide')
-      })
-   })
-}
-);
-
-let card8 = document.querySelector('#card8')
-let radioCard8 = document.getElementsByName('8')
-
-console.log(card8);
-console.log(radioCard8);
-
-radioCard8.forEach(radioElement => {
-   radioElement.addEventListener('click', function () {
-      console.log('clicked card');
-      card8.classList.toggle('shuffle')
-      card8.addEventListener('animationend', function () {
-         card8.classList.toggle('hide')
-      })
-   })
-}
-);
-
-let card9 = document.querySelector('#card9')
-let radioCard9 = document.getElementsByName('9')
-
-console.log(card9);
-console.log(radioCard9);
-
-radioCard9.forEach(radioElement => {
-   radioElement.addEventListener('click', function () {
-      console.log('clicked card');
-      card9.classList.toggle('shuffle')
-      card9.addEventListener('animationend', function () {
-         card9.classList.toggle('hide')
-      })
-   })
-}
-);
-
-let card10 = document.querySelector('#card10')
-let radioCard10 = document.getElementsByName('10')
-
-console.log(card10);
-console.log(radioCard10);
-
-radioCard10.forEach(radioElement => {
-   radioElement.addEventListener('click', function () {
-      console.log('clicked card');
-      card10.classList.toggle('shuffle')
-      card10.addEventListener('animationend', function () {
-         card10.classList.toggle('hide')
-      })
-   })
-}
-);
-
-let card11 = document.querySelector('#card11')
-let radioCard11 = document.getElementsByName('11')
-
-console.log(card11);
-console.log(radioCard11);
-
-radioCard11.forEach(radioElement => {
-   radioElement.addEventListener('click', function () {
-      console.log('clicked card');
-      card11.classList.toggle('shuffle')
-      card11.addEventListener('animationend', function () {
-         card11.classList.toggle('hide')
-      })
-   })
-}
-);
-
-let card12 = document.querySelector('#card12')
-let radioCard12 = document.getElementsByName('12')
-
-console.log(card12);
-console.log(radioCard12);
-
-radioCard12.forEach(radioElement => {
-   radioElement.addEventListener('click', function () {
-      console.log('clicked card');
-      card12.classList.toggle('shuffle')
-      card12.addEventListener('animationend', function () {
-         card12.classList.toggle('hide')
-      })
-   })
-}
-);
-
-let card13 = document.querySelector('#card13')
-let radioCard13 = document.getElementsByName('13')
-
-console.log(card13);
-console.log(radioCard13);
-
-radioCard13.forEach(radioElement => {
-   radioElement.addEventListener('click', function () {
-      console.log('clicked card');
-      card13.classList.toggle('shuffle')
-      card13.addEventListener('animationend', function () {
-         card13.classList.toggle('hide')
-      })
-   })
-}
-);
-
-let card14 = document.querySelector('#card14')
-let radioCard14 = document.getElementsByName('14')
-
-console.log(card14);
-console.log(radioCard14);
-
-radioCard14.forEach(radioElement => {
-   radioElement.addEventListener('click', function () {
-      console.log('clicked card');
-      card14.classList.toggle('shuffle')
-      card14.addEventListener('animationend', function () {
-         card14.classList.toggle('hide')
-      })
-   })
-}
-);
-
-let card15 = document.querySelector('#card15')
-let radioCard15 = document.getElementsByName('15')
-
-console.log(card15);
-console.log(radioCard15);
-
-radioCard15.forEach(radioElement => {
-   radioElement.addEventListener('click', function () {
-      console.log('clicked card');
-      card15.classList.toggle('shuffle')
-      card15.addEventListener('animationend', function () {
-         card15.classList.toggle('hide')
+radioCard5.forEach(radioElement => { // for each radio button
+   radioElement.addEventListener('click', function () { // add event listener
+      console.log('clicked card'); // shows clicked card in console (inspect element)
+      card5.classList.toggle('shuffle') // toggle shuffle class
+      card5.addEventListener('animationend', function () { // add event listener
+         card5.classList.toggle('hide') // toggle hide class
       })
    })
 }
